8805df41d05fb2cb9e55e3d04bc377dea5706813
Hammad Akram <hammadakram6223@gmail.com>
2023-07-26 22:02:26 +0500

    JJJ

--- config/index.js
+++ config/index.js
@@ -5,7 +5,7 @@ config();
 
 const ConnectDB = async () => {
 
-mongoose.connect('mongodb+srv://hammadakram6223:YDGtkYGVpNqXxPyW@gocality-db.zn233ki.mongodb.net/?retryWrites=true&w=majority'  ,{useNewUrlParser: true,useUnifiedTopology: true}) 
+mongoose.connect('mongodb+srv://hammadakram6223:YDGtkYGVpNqXxPyW@gocality-db.zn233ki.mongodb.net/?retryWrites=true&w=majority') 
  .then(() => {
     console.log(`Mongodb connected with server`);
   })
