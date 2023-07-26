18e4daafd41076130bd725dffaaed0ee1c1b1e90
Hammad Akram <hammadakram6223@gmail.com>
2023-07-26 19:39:21 +0500

    h

--- config/index.js
+++ config/index.js
@@ -1,9 +1,11 @@
 
 import mongoose from 'mongoose';
+import { config } from 'dotenv';
+config();
 
 const ConnectDB = async () => {
 
-mongoose.connect('mongodb+srv://hammadakram6223:YDGtkYGVpNqXxPyW@gocality-db.zn233ki.mongodb.net/?retryWrites=true&w=majority',  {useNewUrlParser: true,useUnifiedTopology: true}) 
+mongoose.connect(process.env.DB_URL  ,{useNewUrlParser: true,useUnifiedTopology: true}) 
  .then(() => {
     console.log(`Mongodb connected with server`);
   })
