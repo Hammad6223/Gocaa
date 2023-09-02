963e721e916e57dbf38941fbc08de2a995ac4e4f
Hammad Akram <hammadakram6223@gmail.com>
2023-09-02 15:50:09 +0500

    xcxc

--- controllers/user/Order.js
+++ controllers/user/Order.js
@@ -9,18 +9,15 @@ const Order = {
     inprogress :async  (req,resp,next)=>{
   
 
-   Cart.find({status : 'inprogress',user_id :req.user._id}).select('-vehicle_id').populate('service_id')
+   Cart.find({status : 'inprogress',user_id :req.user._id}).select('-vehicle_id -package_id -user_id').populate('service_id')
    .populate( {path : 'booking_id', populate:{ path : 'vehicle_id' ,populate: { path: 'feature_id',  model: 'Feature'}}} )
    .populate( {path : 'booking_id', populate:{ path : 'driver_id' } }) 
    .populate( {path : 'package_booking_id', populate:{path : 'package_booking_data',populate:{ path : 'vehicle_id' ,populate: { path: 'feature_id',  model: 'Feature'} ,} }} )
    .populate( {path : 'package_booking_id', populate:{path : 'package_booking_data',populate:{ path : 'driver_id' } }} )
-   
+   .populate( {path : 'package_booking_id',populate:{ path : 'package_id' ,populate: { path: 'service_id', }  , select: '-vehicle_id' }  } )
    .exec()
     .then( (data) =>{ return next(new errorHandler(data, 200)); })
     .catch((error) =>{return next(new errorHandler(error.message, 400));  }); 
-
- 
-
 },
 
  pending :async  (req,resp,next)=>{
