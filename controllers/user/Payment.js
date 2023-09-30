912d8bb57cdf911fb96639829735793ea68302d9
Hammad Akram <hammadakram6223@gmail.com>
2023-09-30 11:37:04 +0500

    mfd,f

--- controllers/user/Payment.js
+++ controllers/user/Payment.js
@@ -1,51 +1,52 @@
-// import Stripe from "stripe";
+
 import Cart from "../../models/cart.js";
 import User from '../../models/user.js'
 import Joi from "joi";
 import transaction from "../../models/transaction.js";
 import errorHandler from "../../utills/errorhandler.js";
 
+import Stripe from "stripe";
 // Access the Stripe secret key from the environment variables
-// const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
 
 
-// // Now you can use `stripeSecretKey` in your Stripe client setup
-// const stripeClient = Stripe(stripeSecretKey);
+// Now you can use `stripeSecretKey` in your Stripe client setup
+const stripeClient = Stripe(stripeSecretKey);
 
 const Payment = {
 
   sheet :async  (req,resp,next)=>{
 
-//   const { amount, currency ,order_id} = req.body;
-//   console.log(currency);
+  const { amount, currency ,order_id} = req.body;
+  console.log(currency);
 
-//   const user = await User.findById({ _id: req.user._id }) ;
-//   // Use an existing Customer ID if this is a returning omer.
-//   const customer = await stripeClient.customers.create({ });
-//   const ephemeralKey = await stripeClient.ephemeralKeys.create(
-//     { customer: customer.id },
-//     { apiVersion: '2023-08-16'},
-//   );
-//   try {
-//     const paymentIntent = await stripeClient.paymentIntents.create({
-//       amount: amount,
-//       currency: currency,
-//       customer: customer.id,
-//       payment_method_types: ['card'], // Specify the payment method types you accept, e.g., 'card'
+  const user = await User.findById({ _id: req.user._id }) ;
+  // Use an existing Customer ID if this is a returning omer.
+  const customer = await stripeClient.customers.create({ });
+  const ephemeralKey = await stripeClient.ephemeralKeys.create(
+    { customer: customer.id },
+    { apiVersion: '2023-08-16'},
+  );
+  try {
+    const paymentIntent = await stripeClient.paymentIntents.create({
+      amount: amount,
+      currency: currency,
+      customer: customer.id,
+      payment_method_types: ['card'], // Specify the payment method types you accept, e.g., 'card'
 
-//     });
-//    if(paymentIntent){ Cart.findByIdAndUpdate( { _id: order_id }, {  status: 'onBoarding' }).exec()
-// }
-//     resp.json({
-//       paymentIntent: paymentIntent.client_secret,
-//       ephemeralKey: ephemeralKey.secret,
-//       customer: customer.id,
-//     });
-//   } catch (error) {
-//     // Handle the error
+    });
+   if(paymentIntent){ Cart.findByIdAndUpdate( { _id: order_id }, {  status: 'onBoarding' }).exec()
+}
+    resp.json({
+      paymentIntent: paymentIntent.client_secret,
+      ephemeralKey: ephemeralKey.secret,
+      customer: customer.id,
+    });
+  } catch (error) {
+    // Handle the error
    
-//     resp.status(500).json({ error: 'An error occurred while creating the Payment Intent.' });
-//   }
+    resp.status(500).json({ error: 'An error occurred while creating the Payment Intent.' });
+  }
 },
 
 
